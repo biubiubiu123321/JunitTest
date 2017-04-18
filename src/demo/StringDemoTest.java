8cbca70df022c63d0578c15139df4e7c459a6e27
biubiubiu123321 <1914417081@qq.com>
2017-04-18 10:37:47 +0800

    Signed-off-by: biubiubiu123321 <1914417081@qq.com>

---
+++ src/demo/StringDemoTest.java
@@ -0,0 +1,60 @@
+package demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringDemoTest {
+
+	private StringDemo str;
+	@Before
+	public void setUp() throws Exception {
+		str = new StringDemo();
+	}
+
+	@Test
+	public void testSmallString() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testSmallString2() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		//fail("Not yet implemented");
+	}@Test
+	public void testSmallString3() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		//fail("Not yet implemented");
+	}@Test
+	public void testSmallString4() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		//fail("Not yet implemented");
+	}@Test
+	public void testSmallString5() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		//fail("Not yet implemented");
+	}@Test
+	public void testSmallString6() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		//fail("Not yet implemented");
+	}@Test
+	public void testSmallString7() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		//fail("Not yet implemented");
+	}@Test
+	public void testSmallString8() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testSmallString9() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		//fail("Not yet implemented");
+	}@Test
+	public void testSmallString10() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		//fail("Not yet implemented");
+	}
+
+}
